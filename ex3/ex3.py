8dfbeeed8692259aa55d122d7bccfe8c468f5a90
Robert <robertalphanor@gmail.com>
2025-03-25 13:13:21 +0100

    Selction sort et correction de bubble sort

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,21 +1,48 @@
 #Bubble sort
-tab =  [60, 9, 50, 3, 5]
+tab =  [60, 9, 50, 3, 5, 2]
+
+print("Au début", tab)
 
 def bubble(tab):
     size = len(tab)
-
     for i in range(size - 1):
-        for j in range(size - 1):
-            k = i + j
-            if k < size and k + 1 < size:
-                if tab[k] > tab[k + 1]:
-                    max = tab[k]
-                    min = tab[k + 1]
-                    tab[k] = min
-                    tab[k + 1] = max
+        print("------------------")
+        for j in range(size - i):
+            # k = i + j
+            print(j)
+            if j < size and j + 1 < size:
+                if tab[j] > tab[j + 1]:
+                    max = tab[j]
+                    min = tab[j + 1]
+                    tab[j] = min
+                    tab[j + 1] = max
     return tab
 
-print(bubble(tab))
+print("Bubble sort", bubble(tab))
+
 
 #Selection Sort
+def selection(tab):
+    size = len(tab)
+
+    l = None
+    currentMin = None
+    for i in range(size):
+        for j in range(i + 1, size):
+            if tab[j] < tab[i] and ((currentMin == None ) or (tab[j] < currentMin)):
+                currentMin = tab[j]
+                l = j
+        if l != None:
+            max = tab[i] 
+            min = tab[l]
+            tab[i] = min
+            tab[l] = max
+        l = None
+        currentMin = None
+
+    return tab
+
+print("Selection sort", selection(tab))
+# x = range(3, 6)
+# print(list(x))
 
